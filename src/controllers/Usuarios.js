720966cdb2266fbcdc0384af9f458bcfc28aee64
Ricardo Suarez Briones <ricardo_suarez93@hotmail.com>
2023-06-03 01:17:38 -0500

    UPDateing

--- src/controllers/Usuarios.js
+++ src/controllers/Usuarios.js
@@ -1,12 +1,12 @@
 const sql = require('mssql')
 
 const GetUsuarioLogin = async (req, res) => {
-  const { Login } = req.query
+  const { Login, PassWord } = req.query
 
   console.log(Login)
 
   try {
-    const query = `select * from SEG_Usuario where Login = '${Login}'`
+    const query = `select * from SEG_Usuario where Login = '${Login}' AND PassWord = ${PassWord}`
     const result = await sql.query(query)
     res.status(200).send(result.recordsets[0])
   } catch (error) {
